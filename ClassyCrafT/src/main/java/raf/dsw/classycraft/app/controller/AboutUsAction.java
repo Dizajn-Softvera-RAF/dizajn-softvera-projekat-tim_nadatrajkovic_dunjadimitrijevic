8c2608665d863e4ad99e3c1ecd1c4259a889e87b
duuunjaaa <62660866+duuunjaaa@users.noreply.github.com>
2023-11-05 23:01:39 +0100

    samo dodata AboutUsAction klasa

---
+++ ClassyCrafT/src/main/java/raf/dsw/classycraft/app/controller/AboutUsAction.java
@@ -0,0 +1,17 @@
+package raf.dsw.classycraft.app.controller;
+
+import java.awt.event.ActionEvent;
+
+public class AboutUsAction extends AbstractClassyAction{
+
+    public AboutUsAction()
+    {
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //Pokretanjem te akcije (klikom na opciju iz menija) aplikacija treba da izbaci novi prozor koji će sadržati imena i prezimena studenata koji rade na
+        //projektu kao i njihove slike raspoređene na vama željeni način
+
+    }
+}
