2dc24276b75d7a415b2ee79e3ce1257f55d7e44e
duuunjaaa <62660866+duuunjaaa@users.noreply.github.com>
2023-12-12 20:14:51 +0100

    radi lasso valjda

--- ClassyCrafT/src/main/java/raf/dsw/classycraft/app/controller/stateSablon/SelektujState.java
+++ ClassyCrafT/src/main/java/raf/dsw/classycraft/app/controller/stateSablon/SelektujState.java
@@ -1,15 +1,15 @@
 package raf.dsw.classycraft.app.controller.stateSablon;
 
-import raf.dsw.classycraft.app.model.composite_abstraction.ClassyNode;
-import raf.dsw.classycraft.app.model.composite_implementation.diagramElementi.DiagramElement;
 import raf.dsw.classycraft.app.view.DiagramView;
 import raf.dsw.classycraft.app.view.painteri.ElementPainter;
 
 import java.awt.*;
 
 public class SelektujState implements State{
+    private Point pocetnaTacka;
     @Override
     public void misPritisnut(Point P, DiagramView dv) {
+        pocetnaTacka = P;
         dv.ukloniSveIzSelektovanih(); // prvo isprazni listu selektovani
         for (ElementPainter ep:dv.getPainterList()) {
             if(ep.elementAt(P))
@@ -28,10 +28,23 @@ public class SelektujState implements State{
     @Override
     public void misPovucen(Point P, DiagramView dv) {
         System.out.println("usao u MIS POVUCEN");
+        if(pocetnaTacka != null)
+        {
+            dv.drawLasso(pocetnaTacka, P);
+//            dv.ukloniSveIzSelektovanih(); // prvo isprazni listu selektovani
+//            for (ElementPainter ep:dv.getPainterList()) {
+//                if(ep.elementAt(P))
+//                {
+//                    dv.dodajUSelektovane(ep); // ako je kliknut neki element, dodace ga u listu
+//                    System.out.println("naso selektovan "+ep.toString());
+//                    return;
+//                }
+//            }
+        }
     }
 
     @Override
     public void misOtpusten(Point P, DiagramView dv) {
-
+        dv.removeLasso();
     }
 }
--- ClassyCrafT/src/main/java/raf/dsw/classycraft/app/view/DiagramView.java
+++ ClassyCrafT/src/main/java/raf/dsw/classycraft/app/view/DiagramView.java
@@ -1,19 +1,14 @@
 package raf.dsw.classycraft.app.view;
 
+import jdk.jfr.consumer.RecordedClass;
 import raf.dsw.classycraft.app.Observer.ISubscriber;
-import raf.dsw.classycraft.app.Observer.Notification;
 import raf.dsw.classycraft.app.controller.DiagramViewMouseListener;
-import raf.dsw.classycraft.app.controller.OpenPackageAction;
 import raf.dsw.classycraft.app.model.composite_implementation.Diagram;
-import raf.dsw.classycraft.app.model.composite_implementation.diagramElementi.DiagramElement;
-import raf.dsw.classycraft.app.model.composite_implementation.diagramElementi.Interclass;
 import raf.dsw.classycraft.app.view.painteri.ElementPainter;
+import raf.dsw.classycraft.app.view.painteri.InterclassPainter;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,12 +59,125 @@ public class DiagramView extends JPanel implements ISubscriber {
         ep.setSelektovano(true);
         repaint();
     }
+    private void ukloniIzSelektovanih(ElementPainter ep)
+    {
+        ep.setSelektovano(false);
+        this.selektovaniList.remove(ep);
+        //repaint(); // mozda i ne?
+    }
     public void ukloniSveIzSelektovanih()
     {
         for (ElementPainter ep:selektovaniList) {
             ep.setSelektovano(false);
         }
         selektovaniList.clear();
+        repaint();
+    }
+
+    private boolean crtaSeLaso;
+    private Rectangle laso;
+
+    private boolean doOverlap(Point l1, Point r1, Point l2, Point r2)
+    {
+        System.out.println("l1: " + l1+" r1: "+ r1 + ";  l2: "+l2 + " r2: " + r2);
+        // if rectangle has area 0, no overlap
+        if (l1.x == r1.x || l1.y == r1.y || r2.x == l2.x || l2.y == r2.y)
+        {
+            System.out.println("prvi uslov");
+            return false;
+        }
+
+        // If one rectangle is on left side of other
+        if (l1.x > r2.x || l2.x > r1.x) {
+            System.out.println("drugi uslov");
+            return false;
+        }
+
+        // If one rectangle is above other
+//        if (r1.y > l2.y || r2.y > l1.y) {
+//            System.out.println("treci uslov");
+//            return false;
+//        }
+
+        // If one rectangle is above other
+        if (r1.y < l2.y || r2.y < l1.y) {
+            System.out.println("treci uslov");
+            return false;
+        }
+
+        return true;
+    }
+    public void drawLasso(Point p1, Point p2)
+    {
+        Point gorelevo;
+        Point doledesno;
+        if(p1.x < p2.x && p1.y < p2.y)
+        {
+            gorelevo = p1;
+            doledesno = p2;
+        }
+        else if(p1.x > p2.x && p1.y > p2.y)
+        {
+            gorelevo = p2;
+            doledesno = p1;
+        }
+        else if(p1.x > p2.x && p1.y < p2.y)
+        {
+            gorelevo = new Point(p2.x, p1.y);
+            doledesno = new Point(p1.x, p2.y);
+        }
+        else/* if(p1.x < p2.x && p1.y > p2.y)*/
+        {
+            gorelevo = new Point(p1.x, p2.y);
+            doledesno = new Point(p2.x, p1.y);
+        }
+        int width = doledesno.x - gorelevo.x;
+        int height = doledesno.y - gorelevo.y;
+        crtaSeLaso = true;
+        laso = new Rectangle(gorelevo.x, gorelevo.y,width, height);
+        for(ElementPainter ep:painterList)
+        {
+            System.out.println("trenutni painter " + ep.getDiagramElement().getName());
+            if(ep instanceof InterclassPainter)
+            {
+                System.out.println("ep je instanceof InterclassPainter");
+                Point l1 = ((InterclassPainter) ep).getPocetnaTacka();
+                Point r1 = ((InterclassPainter) ep).getKrajnjaTacka();
+                System.out.println("l1: " + l1+" r1: "+ r1);
+                if(doOverlap(l1, r1, gorelevo, doledesno)) // zasto ne radi:((
+                {
+                    System.out.println("they DO OVERLAP");
+                    this.dodajUSelektovane(ep);
+                }
+                else
+                {
+                    System.out.println("they DONT overlap");
+                    this.ukloniIzSelektovanih(ep);
+                }
+            }
+
+//            if(ep.elementAt(p2))
+//            {
+//                this.dodajUSelektovane(ep); // ako je kliknut neki element, dodace ga u listu
+//                //System.out.println("naso selektovan "+ep.toString());
+//                return;
+//            }
+        }
+//        for (ElementPainter ep:painterList) {
+//            //if(ep.elementAt(p2))
+////            {
+////                dv.dodajUSelektovane(ep); // ako je kliknut neki element, dodace ga u listu
+////                System.out.println("naso selektovan "+ep.toString());
+////                return;
+////            }
+//        }
+        //g.drawRect(p1.x,p1.y,width ,height);
+        repaint();
+    }
+    public void removeLasso()
+    {
+        // trebace posle
+        crtaSeLaso = false;
         repaint();
     }
     private ElementPainter selektovan;
@@ -94,6 +202,8 @@ public class DiagramView extends JPanel implements ISubscriber {
         diagramListener=new DiagramViewMouseListener(this);
         painterList=new ArrayList<>();
         selektovaniList = new ArrayList<>();
+
+        crtaSeLaso = false;
 
         this.addMouseListener(diagramListener);
         this.addMouseMotionListener(diagramListener);
@@ -130,7 +240,8 @@ public class DiagramView extends JPanel implements ISubscriber {
         for (ElementPainter p:painterList) {
             p.draw(g2);
         }
-
+        if(crtaSeLaso)
+            g2.draw(laso);
         mouseDrag(g2);
 
         //ovde crta liniju za veze
